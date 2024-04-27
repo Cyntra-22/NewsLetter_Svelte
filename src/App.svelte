d71b7a957ec75bc80e098cea7bdfbed30b84e3d1
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-04-28 01:17:32 +0700

    Created the frame for newsletter

--- src/App.svelte
+++ src/App.svelte
@@ -5,3 +5,25 @@
 <style>
 	
 </style>
+
+<div class="main-container">
+	<div class="left-container">
+		<div class="">
+			<h1>Stay updated!</h1>
+			<p>Join 60,000+ product managers receiving monthly updates on: </p>
+			<ul>
+				<li><img src="/" alt=""/>Product discovery and building what matters</li>
+				<li><img src="/" alt=""/>Measuring to ensure updates are a success </li>
+				<li><img src="/" alt=""/> And much more!</li>
+			</ul>
+		</div>
+
+		<div class="">
+			<button>Subscribe to monthly newsletter</button>
+		</div>
+	</div>
+	<div class="right-container">
+		
+	</div>
+
+</div>
