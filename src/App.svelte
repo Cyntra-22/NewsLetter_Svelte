0e85ca77c1349d5548e5dc0a788b341f842d1c58
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-04-28 02:57:09 +0700

    Styled the component

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,20 @@
 </script>
 
 <style>
+
+	:root{
+		--font:'Work Sans', sans-serif;
+		--bgColor: rgb(132, 68, 210);
+
+	}
+
+	:global(body){
+		padding: 0;
+		margin: 0;
+		background-color: var(--bgColor);
+		font-family: var(--font);   	 
+	}
+
 	.main-container{
 		display: flex;
 		width: 40%;
@@ -10,14 +24,33 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%,-50%);
-		background-color: blueviolet;
+		background-color: white;
 		padding: 20px;
 		border-radius: 15px;
 		justify-content: space-between;
 	}
 
+	.left-container{
+		padding-right: 20px;
+		padding-left: 10px;
+	}
+
 	.right-container img{
 		width: 270px;
+	}
+	
+	ul{
+		padding: 0;
+	}
+	li{
+		list-style-type: none;
+		padding-top: 7px;
+		padding-bottom: 7px;
+	}
+
+	.list-item{
+		padding-left: 7px;
+		font-size: 14px;
 	}
 </style>
 
@@ -27,9 +60,9 @@
 			<h1>Stay updated!</h1>
 			<p>Join 60,000+ product managers receiving monthly updates on: </p>
 			<ul>
-				<li><img src="/" alt=""/>Product discovery and building what matters</li>
-				<li><img src="/" alt=""/>Measuring to ensure updates are a success </li>
-				<li><img src="/" alt=""/> And much more!</li>
+				<li><img src="/icon-list.svg" alt="" width="15px"/><span class="list-item">Product discovery and building what matters</span></li>
+				<li><img src="/icon-list.svg" alt="" width="15px"/><span class="list-item">Measuring to ensure updates are a success </span></li>
+				<li><img src="/icon-list.svg" alt="" width="15px"/><span class="list-item">And much more!</span></li>
 			</ul>
 		</div>
 
